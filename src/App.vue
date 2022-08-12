39b3748ab9d738d9e8d8b039aa5f5dbe5064873b
Minkyu3 <gkhtls2006@broawdwave.co.kr>
2022-08-13 00:37:00 +0900

    방이미지 목록, 포트번호 수정, 추천버튼 기능 추가, 계약기간 텍스트추가

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,23 @@
 <template>
   <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
-
+  <div class="black-bg" v-if="modalbool == true">
+    <div class="white-bg">
+      <h4>상세페이지제목임</h4>
+      <p>상세페이지내용임</p>
+      <button @click = "modalbool = false">닫기</button>
+    </div>
+  </div>
+  <h1>큰빛빌라</h1>
   <div class="menu">
     <a v-for="menuName in menuLists" :key="menuName" >{{ menuName }}</a>
   </div>
 
   <div v-for="(roomName,i) in roomLists" :key="i">
-    <p>{{ roomLists[i] }}</p>
+    <img @click = "modalbool = true" class="room-img" src='./assets/room0.jpg'> 
+    <h4>{{ roomLists[i] }}</h4>
     <p>{{ states[i] }}</p>
+    <p v-if="startDate[i] != null">기간 : {{startDate[i]}} ~ {{endDate[i]}}</p>
+    <button @click="suggestion[i]++">추천하기</button><span>추천수 : {{ suggestion[i] }}</span>
   </div>
 
 </template>
@@ -18,11 +28,23 @@ export default {
   name: 'App',
    data(){
     return {
+      modalbool : false,
+      imagesLists : [
+        './assets/room0.jpg','./assets/room1.jpg','./assets/room2.jpg','./assets/room3.jpg','./assets/room4.jpg'
+      ],
       menuLists : ['Home', 'Room', 'About'],
       roomLists : ['101호', '102호', '103호', '103호', '105호'],
       states : ['계약중', '계약중', '빈방', '빈방', '계약중'],
+      suggestion : [0, 0, 0, 0, 0],
+      startDate : ['2022-05-10', '2022-01-01', null, null, '2022-03-10'],
+      endDate : ['2023-01-01', '2022-12-10', null, null, '2022-10-03'],
     }
   },
+
+  methods : {
+    
+  },
+
   components: {
 
   }
@@ -48,5 +70,27 @@ export default {
   color : white;
   padding : 10px;
 }
+
+.room-img {
+  width: 100%;
+  margin-top: 40px;
+}
+
+body {
+  margin : 0;
+}
+div {
+  box-sizing: border-box;
+}
+.black-bg {
+  width: 100%; height:100%;
+  background: rgba(0,0,0,0.5);
+  position: fixed; padding: 20px;
+}
+.white-bg {
+  width: 100%; background: white;
+  border-radius: 8px;
+  padding: 20px;
+} 
 
 </style>
